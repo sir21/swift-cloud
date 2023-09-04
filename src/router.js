3997eff94dc249e661e8c4320dd65f3275c1d1b0
sir21 <sasitha.iresha@gmail.com>
2023-09-05 07:37:03 +0800

    Koa setup

---
+++ src/router.js
@@ -0,0 +1,8 @@
+const Router = require("koa-router");
+const router = new Router();
+
+// Add routers
+// router.get("/events_list", getEvents);
+// router.post("/add_event", addEvent);
+
+module.exports = router;
